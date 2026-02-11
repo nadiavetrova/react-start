a9a372a61e15b1055f407d60be0a6e2cd65211f1
nadiavetrova <152676329+nadiavetrova@users.noreply.github.com>
2026-02-11 23:36:54 +0800

    add simple JSX

---
+++ src/components/CostItem.js
@@ -0,0 +1,13 @@
+function CostItem() {
+  return (
+    <div>
+      <div>март 2026</div>
+      <div>
+        <h2>Холодильник</h2>
+        <div>$999.99</div>
+      </div>
+    </div>
+  );
+}
+
+export default CostItem;
