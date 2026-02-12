12e6de667cb8bc8c673e6afd59de8ddc01ebfe88
nadiavetrova <152676329+nadiavetrova@users.noreply.github.com>
2026-02-12 21:46:57 +0800

    Отображение Динамических Данных в JSX

--- src/components/CostItem.js
+++ src/components/CostItem.js
@@ -1,12 +1,16 @@
 import "./CostItem.css";
 
 function CostItem() {
+  const costDate = new Date(2026, 1, 12);
+  const costDescription = "Холодильник";
+  const costAmount = 999.99;
+
   return (
     <div className="cost-item">
-      <div>март 2026</div>
+      <div>{costDate.toISOString()}</div>
       <div>
-        <h2 className="cost-item__description">Холодильник</h2>
-        <div className="cost-item__price">$999.99</div>
+        <h2 className="cost-item__description">{costDescription}</h2>
+        <div className="cost-item__price">${costAmount}</div>
       </div>
     </div>
   );
