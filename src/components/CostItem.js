96a24481721101df2005a3b586e91791b3256565
nadiavetrova <152676329+nadiavetrova@users.noreply.github.com>
2026-02-13 19:49:20 +0800

    Передача Данных при Помощи props

--- src/components/CostItem.js
+++ src/components/CostItem.js
@@ -1,16 +1,12 @@
 import "./CostItem.css";
 
-function CostItem() {
-  const costDate = new Date(2026, 1, 12);
-  const costDescription = "Холодильник";
-  const costAmount = 999.99;
-
+function CostItem(props) {
   return (
     <div className="cost-item">
-      <div>{costDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div>
-        <h2 className="cost-item__description">{costDescription}</h2>
-        <div className="cost-item__price">${costAmount}</div>
+        <h2 className="cost-item__description">{props.description}</h2>
+        <div className="cost-item__price">${props.amount}</div>
       </div>
     </div>
   );
