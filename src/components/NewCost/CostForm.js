70af36568113079f6ef08a7cc5fde076d4afa4e6
Nadezhda <nadi@MacBook-Pro-Nadezhda.local>
2026-02-19 18:45:33 +0800

    Работа с Несколькими Состояниями

--- src/components/NewCost/CostForm.js
+++ src/components/NewCost/CostForm.js
@@ -1,11 +1,25 @@
+import React, {useState} from "react"; 
 import "./CostForm.css";
 
 const CostForm = () => {
 
+const [name, setName] = useState('');
+const [amount, setAmount] = useState('');
+const [date, setDate] = useState('');
 
 const nameChangeHandler = (event) => {
+  setName(event.target.value);
   console.log(event.target.value);
-  
+}
+
+const amountChangeHandler = (event) => {
+  setAmount(event.target.value);
+  console.log(event.target.value);
+}
+
+const dateChangeHandler = (event) => {
+    setDate(event.target.value);
+  console.log(event.target.value);
 }
 
 return   <form>
@@ -16,11 +30,11 @@ return   <form>
     </div>
         <div className="new-cost__control">
       <label>Сумма</label>
-      <input type="number" min='0.01' step='0.01'/>
+      <input type="number" min='0.01' step='0.01' onChange={amountChangeHandler}/>
     </div>
         <div className="new-cost__control">
       <label>Дата</label>
-      <input type="date" min='2019-01-01' max='2022-01-01'/>
+      <input type="date" min='2026-01-01' max='2026-02-19' onChange={dateChangeHandler}/>
     </div>
     <div className="new-cost__actions">
 <button type='submit'>Добавить расход </button>
